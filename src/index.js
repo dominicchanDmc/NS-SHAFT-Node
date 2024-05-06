c441b7f47a8e0b7ae47012ce4282d6155d5d13a9
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-05-06 16:20:41 -0700

    display success

--- src/index.js
+++ src/index.js
@@ -14,26 +14,29 @@ const blockpic=new Image();
 		blockpic.src="./assets/block.jpg";
 let canvasB,ctxB,canvasM,ctxM;
 
+
+
 let gameStatus = statusSt;
 let player = new PlayerObj();
 
-canvasB = document.getElementById('canvasBoard');
-ctxB = canvasB.getContext('2d');
-canvasM = document.getElementById('canvasMove'); 
-ctxM = canvasM.getContext('2d');
-canvasB.width = 480;
-canvasB.height = 740;
-canvasM.width = 480;
-canvasM.height = 300;
+
 //-------------------------
 function render() {
+    canvasB = document.getElementById('canvasBoard');
+    ctxB = canvasB.getContext('2d');
+    canvasM = document.getElementById('canvasMove'); 
+    ctxM = canvasM.getContext('2d');
+    canvasB.width = 480;
+    canvasB.height = 740;
+    canvasM.width = 480;
+    canvasM.height = 300;
     if(gameStatus == statusSt ){
         ctxB.drawImage(bgpic,0,0,canvasB.width,canvasB.height);
         if(player.state == 0 ) 
             ctxB.drawImage(stand,player.x,player.y,player.width,player.height);
 
         ctxB.drawImage(toppic,0,0,canvasB.width,40);
-        for(i = 0 ; i < 10 ; i++){
+        for(let i = 0 ; i < 10 ; i++){
             if(Array[i].mod == 0) ctxB.drawImage(blockpic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
             else if  (Array[i].mod == 1) ctxB.drawImage(spic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
             else if  (Array[i].mod == 2) ctxB.drawImage(kpic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
@@ -52,18 +55,23 @@ function run() {
     time = Date.now();
 }
 
+var time = Date.now();
+setInterval(run, 40);
+
 let Array = [];
 Array[0] = new BlockObj(150,600);
 for( let i = 1 ; i < 10 ; i++){
-    let r = Math.random() * canvasB.width-100;
+    // let r = Math.random() * canvasB.width-100;
+    let r = Math.random() * 380;
     let de = (Math.random() * 10+10+i*4)*40 ;
     /// Avoid Overlapping
     while(1)
     {
-        r = Math.random() * canvasB.width-100;
+        // r = Math.random() * canvasB.width-100;
+        r = Math.random() * 380;
         de = (Math.random() * 10+10+i*4)*40 ;
         let overlap = false;
-        for(j = 0 ; j < i ; j++)
+        for(let j = 0 ; j < i ; j++)
         {
             if(i==j) continue;
             if( (Math.abs(r-Array[j].x)<=200) && (Math.abs(de-Array[j].y)<=60)  )
@@ -80,7 +88,8 @@ for( let i = 1 ; i < 10 ; i++){
     // else if ( i == 2 || i == 7) Array[i] = new k_block(r,de);
     // else if ( i == 5 ) Array[i] = new j_block(r,de);
     // else Array[i] = new BlockObj(r,de);
+    if(i == 3 || i == 9 ) Array[i] = new BlockObj(r,de);
+    else if ( i == 2 || i == 7) Array[i] = new BlockObj(r,de);
+    else if ( i == 5 ) Array[i] = new BlockObj(r,de);
+    else Array[i] = new BlockObj(r,de);
 }
-
-var time = Date.now();
-setInterval(run, 40);
