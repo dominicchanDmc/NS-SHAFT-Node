3ac328a1583ca6cfc053e8d98950873d1d8bb128
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-05-08 18:39:05 -0700

    clean code

--- src/script/block.js
+++ src/script/block.js
@@ -1,13 +1,3 @@
-// function block(a,b){
-//     this.x= a;
-//     this.y= b;
-//     this.width= 150;
-//     this.height= 30;
-//     this.mod = 0;
-//     this.color = '#198e99';
-// };
-
-
 class BlockObj{
     
     constructor(a,b) {
