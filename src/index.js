f9bf911877fd7d4749feeb2fc7d118f84a396e41
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-05-08 18:30:59 -0700

    build new block

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import PlayerObj from "./script/player.js"
 import BlockObj from "./script/block.js";
+import J_BlockObj from "./script/j_Block.js";
 
 //--------param
 const statusSt = 'Start';
@@ -10,8 +11,14 @@ const toppic=new Image();
 		 toppic.src="./assets/top.jpg";
 const stand=new Image();
 		 stand.src="./assets/1.png";
-const blockpic=new Image();
-		blockpic.src="./assets/block.jpg";
+const blockPic=new Image();
+		blockPic.src="./assets/block.jpg";
+const j_blockPic=new Image();
+		j_blockPic.src="./assets/jblock.jpg";
+const leftpic=new Image();
+		leftpic.src="./assets/left.png";
+const rightpic=new Image();
+		rightpic.src="./assets/right.png";
 let canvasB,ctxB,canvasM,ctxM;
 
 
@@ -34,19 +41,29 @@ function render() {
         ctxB.drawImage(bgpic,0,0,canvasB.width,canvasB.height);
         if(player.state == 0 ) 
             ctxB.drawImage(stand,player.x,player.y,player.width,player.height);
-
+        else if(player.state == 1){
+            ctxB.drawImage(leftpic,a,0,player.width,player.height,player.x,player.y,player.width,player.height);
+            a+=50;
+            if(a >= 200) a =0;
+        }	
+        else if(player.state == 2){
+            ctxB.drawImage(rightpic,b,0,player.width,player.height,player.x,player.y,player.width,player.height);
+            b +=50;
+            if(b >= 200) b =0;
+        }
         ctxB.drawImage(toppic,0,0,canvasB.width,40);
         for(let i = 0 ; i < 10 ; i++){
-            if(Array[i].mod == 0) ctxB.drawImage(blockpic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
+            if(Array[i].mod == 0) ctxB.drawImage(blockPic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
             else if  (Array[i].mod == 1) ctxB.drawImage(spic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
             else if  (Array[i].mod == 2) ctxB.drawImage(kpic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
-            else if  (Array[i].mod == 3) ctxB.drawImage(jpic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
+            else if  (Array[i].mod == 3) ctxB.drawImage(j_blockPic,Array[i].x, Array[i].y, Array[i].width, Array[i].height);
             else{
                 ctxB.fillStyle = Array[i].color;
                 ctxB.fillRect(Array[i].x, Array[i].y, Array[i].width, Array[i].height);
             }	
         }
     }
+    
 }
 
 function run() {
@@ -90,6 +107,6 @@ for( let i = 1 ; i < 10 ; i++){
     // else Array[i] = new BlockObj(r,de);
     if(i == 3 || i == 9 ) Array[i] = new BlockObj(r,de);
     else if ( i == 2 || i == 7) Array[i] = new BlockObj(r,de);
-    else if ( i == 5 ) Array[i] = new BlockObj(r,de);
+    else if ( i == 5 ) Array[i] = new J_BlockObj(r,de);
     else Array[i] = new BlockObj(r,de);
 }
--- src/script/j_Block.js
+++ src/script/j_Block.js
@@ -1,10 +1,12 @@
-class J_BlockObj(a,b){
-    this.x= a;
-    this.y= b;
-    this.width= 150;
-    this.height= 30;
-    this.mod = 3;
-    this.color = '#FFFF00';
+class J_BlockObj{
+    constructor(a,b) {
+        this.x= a;
+        this.y= b;
+        this.width= 150;
+        this.height= 30;
+        this.mod = 3;
+        this.color = '#FFFF00';
+    }
 };
 
 export default J_BlockObj; 
