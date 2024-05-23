01742c6c08bb9f8d0712d6df99b676617fd59dc7
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-05-23 00:38:10 -0700

    tool block

--- src/script/j_Block.js
+++ src/script/j_Block.js
@@ -2,7 +2,7 @@ class J_BlockObj{
     constructor(a,b) {
         this.x= a;
         this.y= b;
-        this.width= 150;
+        this.width= 130;
         this.height= 30;
         this.mod = 3;
         this.color = '#FFFF00';
