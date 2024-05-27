b4b4ede8673a359288916e9a6001f7d2f2e217e1
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-05-27 16:13:50 -0700

    add z-block

---
+++ src/script/block_z.js
@@ -0,0 +1,13 @@
+class BlockZObj{
+    
+    constructor(a,b) {
+        this.x= a;
+        this.y= b;
+        this.width= 300;
+        this.height= 300;
+        this.mod = 0;
+        this.color = '#198e99';
+    }
+} 
+
+export default BlockZObj; 
