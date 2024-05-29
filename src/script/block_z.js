800c0e63959e644ee72f3d72c80091dda7228594
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-05-29 02:05:00 -0700

    add new mode

--- src/script/block_z.js
+++ src/script/block_z.js
@@ -8,6 +8,16 @@ class BlockZObj{
         this.mod = 0;
         this.color = '#198e99';
     }
+
+    constructor(a,b,c) {
+        this.x= a;
+        this.y= b;
+        this.width= 300;
+        this.height= 300;
+        this.mod = 0;
+        this.mode = c;
+        this.color = '#198e99';
+    }
 } 
 
 export default BlockZObj; 
