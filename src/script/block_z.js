f8a4833fcfa694854565a50228cbd6f9cb089a30
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-08-19 11:36:53 -0700

    change block z

--- src/script/block_z.js
+++ src/script/block_z.js
@@ -9,26 +9,25 @@ class BlockZObj{
         this.z = [x,y];
     }
 
-    constructor(a,b,c,d) {
+    constructor(a,b,c,action) {
         this.x= a;
         this.y= b;
         this.width= 300;
         this.height= 300;
         this.mod = 0;
         this.mode = c;
-        this.action = d;
+        this.action = action;
         this.color = '#198e99';
         this.z = [x,y];
     }
 
-    setZValue(a,b,speed,mod,size,hp) {
+    setZValue(a,b,speed,mod,size) {
        this.x= a;
         this.y= b;
         this.speed = speed;
         this.mod= mod;
         this.z = [x,y];
         this.size = size;
-        this.hp = hp;
     }
 
     
