05b198608774214fb79dac7ff98f940cfa1e7ac3
dominicchanDmc <dominicchan.dmc@gmail.com>
2024-09-23 14:02:41 -0700

    add L_block

---
+++ src/script/l_Block.js
@@ -0,0 +1,12 @@
+class L_BlockObj{
+    constructor(a,b) {
+        this.x= a;
+        this.y= b;
+        this.width= 130;
+        this.height= 30;
+        this.mod = 3;
+        this.color = '#FFFF00';
+    }
+};
+
+export default L_BlockObj; 
